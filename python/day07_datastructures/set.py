a3ef8b025707a1218a93125e67a67727425e6ce5
Chaithra P S <chaithraps6804@gmail.com>
2026-01-23 21:24:39 +0530

    Add set operations example in set.py
    Demonstrate set operations including union, intersection, and difference.

---
+++ python/day07_datastructures/set.py
@@ -0,0 +1,8 @@
+my_fruits={'Apple','banana','peach','papaya'}
+her_fruits={'tomatoes','pineapples','papaya','peach'}
+union_result=my_fruits.union(her_fruits)
+intersection_set=my_fruits.intersection(her_fruits)
+difference_set=my_fruits.difference(her_fruits)
+print(union_result)
+print(intersection_set)
+print(difference_set)
